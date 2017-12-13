f554591075492840cfc32fd1fa37ca9be730d84f
Brandon Pollack <brpol@microsoft.com>
2017-12-13 14:52:49 -0800

    changed email for header

--- grok3d/src/System/SystemManager.cpp
+++ grok3d/src/System/SystemManager.cpp
@@ -1,5 +1,5 @@
 /* Copyright (c) 2017 Brandon Pollack
-* Contact @ grok3d@gmail.com
+* Contact @ grok3dengine@gmail.com
 * This file is available under the MIT license included in the project
 */
 #include "System/SystemManager.h"
